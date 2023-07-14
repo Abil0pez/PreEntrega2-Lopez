6fb1a757a629cac2016c05e4ac897a1da78ef66a
Abil0pez <131202931+Abil0pez@users.noreply.github.com>
2023-07-13 21:23:18 -0300

    app.js

--- app.js
+++ app.js
@@ -1,59 +1,78 @@
-class Producto {
-    nombre;
-    precio;
-
-    constructor(nombre, precio) {
-        this.nombre = nombre;
-        this.precio = precio;
+function agregar0(){
+    let objeto = JSON.parse(localStorage.getItem("productos"))
+    let producto = objeto[0].precio
+    let carrito = []
+    if (JSON.parse(localStorage.getItem("carrito"))){
+        carrito = JSON.parse(localStorage.getItem("carrito"))
     }
+    carrito.push(JSON.stringify(producto))
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    let padre = document.getElementById("lista")
+    let li = document.createElement("li")
+    li.innerHTML = objeto[0].nombre
+    padre.appendChild(li)
 }
 
-function main() {
-    const producto1 = new Producto("Alaska", 5999);
-    const producto2 = new Producto("Bilbao", 3999);
-    const producto3 = new Producto("Black", 9999);
-    const producto4 = new Producto("Tokyo", 2999);
-
-    const catalogo = [producto1, producto2, producto3, producto4];
-
-
-
-
-    let seleccion = prompt("Hola desea comprar algun producto si o no");
-
-    while (seleccion != "si" && seleccion != no) {
-        alert("Porfavor ingresa si o no")
-        seleccion = prompt("Hola desea comprar algo si o no")
+function agregar1(){
+    let objeto = JSON.parse(localStorage.getItem("productos"))
+    let producto = objeto[1].precio
+    let carrito = []
+    if (JSON.parse(localStorage.getItem("carrito"))){
+        carrito = JSON.parse(localStorage.getItem("carrito"))
     }
-
-    if (seleccion == "si") {
-        alert("A continuacion nuestra lista de productos")
-        alert("Alaska - 5999 - ID: 1\nBilbao - 3999 - ID: 2\n Black - 9999 - ID: 3\nTokyo - 2999 - ID: 4")
-    } else if (seleccion == "no") {
-        alert("Gracias por visitar la pagina, hasta pronto!")
-    }
-
-    while (seleccion != "no") {
-        let eleccion = parseInt(prompt("Agrega un producto a tu carrito! Ponga su ID"))
-        console.log(eleccion)
-        let id = eleccion - 1
-        if (eleccion = !0) {
-
-            console.log(id)
-            let unidades = parseInt(prompt("Cuantas unidades quiere llevar"))
-
-            let precioFinal = catalogo[id].precio * unidades
-            console.log(precioFinal)
-            alert("El costo es de " + precioFinal)
-        } else {
-            alert("No tenemos ese producto")
-        }
-
-        seleccion = prompt("Desea seguir comprando?")
-
-        if (seleccion === "no")
-            alert("Gracias por su compra! hasta pronto")
-    }
+    carrito.push(JSON.stringify(producto))
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    let padre = document.getElementById("lista")
+    let li = document.createElement("li")
+    li.innerHTML = objeto[1].nombre
+    padre.appendChild(li)
 }
 
-main()
+function borrar(){
+    let padre = document.getElementById("lista")
+    padre.innerHTML = ""
+    let mostrar = document.getElementById("mostrar")
+    mostrar.innerHTML = ""
+    localStorage.removeItem("carrito")
+    localStorage.removeItem("total")
+}
+
+function precioFinal(){
+    
+    localStorage.setItem("total", total)
+}
+
+function mostrarTotal(){
+    let carrito = JSON.parse(localStorage.getItem("carrito"))
+    let precioFinal = 0
+    for (let total of carrito) {
+        
+        precioFinal += parseInt(total)
+    }
+    let h2 = document.createElement("h2")
+    h2.innerHTML = precioFinal
+    let padre = document.getElementById("mostrar")
+    padre.appendChild(h2)
+    localStorage.setItem("total", precioFinal)
+}
+
+
+
+let objeto = [
+                {nombre: "Alaska", id: 0, precio: 2000},
+                {nombre: "Tokyo", id: 1, precio: 3500}]
+
+localStorage.setItem("productos", JSON.stringify(objeto))
+
+
+let alaska = document.getElementById("alaska")
+let tokyo = document.getElementById("tokyo")
+let calcular = document.getElementById("precio")
+let limpiar = document.getElementById("limpiar")
+
+alaska.addEventListener("click", agregar0)
+tokyo.addEventListener("click", agregar1)
+limpiar.addEventListener("click", borrar)
+
+
+calcular.addEventListener("click", mostrarTotal)
